e8b9aad668c9b95441043e49f884a4f6a05208d4
Soren <quvazaray@gmail.com>
2026-09-25 14:40:39 +1000

    style: apply rustfmt to verification infrastructure

--- lambars-verification/src/lib.rs
+++ lambars-verification/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(unsafe_code)]
 
-pub use lambars_verify_macros::{dual_verify, verification_case, VerificationModel};
+pub use lambars_verify_macros::{VerificationModel, dual_verify, verification_case};
 
 /// Marker implemented by data models that participate in generated verification.
 pub trait VerificationModel {
