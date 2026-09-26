27470ab493b549c338a59ff22829b990441c209b
Soren <quvazaray@gmail.com>
2026-09-26 13:31:58 +1000

    safety: forbid unsafe code across the runtime crate

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@
 //! // Example usage will be added as features are implemented
 //! ```
 
-#![deny(unsafe_code)]
+#![forbid(unsafe_code)]
 #![warn(missing_docs)]
 #![warn(clippy::all)]
 #![warn(clippy::pedantic)]
